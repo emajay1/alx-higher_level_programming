0eaec81a829ddb8f86d66018ca49a8e0e5cf51c2
emajay1 <ogbonnayaemmanuel2030@gmail.com>
2022-09-20 20:18:06 -0700

    rasaf

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif i % 3 == 0:
-            print("Fizz ", end="")
-        elif i % 5 == 0:
-            print("Buzz ", end="")
-        else:
-            print("{} ".format(i), end="")
+def uppercase(str):
+    for upper in range(0, len(str)):
+        print("{:c}".format(ord(str[upper])-32)
+                if ord(str[upper]) in range(ord('a'), ord('z')+1)
+                else str[upper], end='')
+    print()
